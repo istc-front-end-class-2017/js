ba788ea7d832966bbdeaebc0c8b6f2aa50ba7207
Narek Ghevandiani <n.ghevandiani@gmail.com>
2017-10-21 13:15:54 +0400

    DOM api

---
+++ 21/script.js
@@ -0,0 +1,31 @@
+let elem = document.getElementById('a');
+// console.log(typeof document.getElementById('a'));
+console.log(elem);
+// console.log(elem.innerHTML);
+
+// setInterval(()=>{
+//     elem.innerHTML += Math.ceil(Math.random()*10);
+// },
+// 100);
+
+
+// let elem1 = document.getElementsByClassName('b');
+// console.log(typeof elem1);
+// console.log(elem1);
+
+// let pTags = document.getElementsByTagName('p');
+// console.log(pTags);
+
+// setTimeout(()=>{
+//     console.log('after 10000 ms');
+// }, 10000);
+
+
+// console.log('hey');
+
+// let myElement = document.createElement('p');
+// myElement.innerHTML = 'my created element';
+// console.log(myElement);
+
+// // let body = document.getElementsByTagName('body')[0];
+// document.body.appendChild(myElement);
